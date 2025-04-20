854a805d79053c7f6868f6dc3883eb644e55a128
Aysha Noor <50948684+ayshanoorr@users.noreply.github.com>
2025-04-20 15:56:35 +0500

    Update main.py
    Link ui to the backend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from app.components.interview import process_interview
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+app.mount("/", StaticFiles(directory=Path(__file__).parent.parent / "ui", html=True), name="ui")
 
 @app.post("/interview")
 async def start_interview(resume: UploadFile, jobdesc: UploadFile, difficulty: str = Form("medium")):
